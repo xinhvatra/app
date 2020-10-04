0e7cae50c8db469bd19fa5b78d2bf75d17fff911
xinhvatra <p.dag.khoa@gmail.com>
2020-10-04 15:52:58 +0700

    home

--- TSBD/TemplateCreator/Form1.cs
+++ TSBD/TemplateCreator/Form1.cs
@@ -32,6 +32,7 @@ namespace TSBD
 		{
 			for (int j = 0; j < dtDuno.Rows.Count - 1; j++) // chạy bảng khách hàng đã phân công theo cán bộ
 			{
+				
 				Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
 				Document doc = new Document();
 				string filename = "";
@@ -47,72 +48,85 @@ namespace TSBD
 						ref missing, ref missing, ref missing, ref missing,
 						ref missing, ref missing, ref missing);
 				count = 0;
-				LDS = giatri = giatritt = mucdichvay = new string[5] { "", "", "", "", "" };
+				LDS = new string[5] { "", "", "", "", "" };
+				 giatri  = new string[5] { "", "", "", "", "" };
+				 mucdichvay = new string[5] { "", "", "", "", "" };
+				 giatritt = new string[5] { "", "", "", "", "" };
 
 				for (int i = 1; i <= doc.Paragraphs.Count; i++)
-				{
-					label2.Text = dtDuno.Rows[j][2].ToString() + "_" + dtDuno.Rows[j][3].ToString() + "_" + dtDuno.Rows[j][0].ToString();
+				{ int dem = j;
+					//label2.Text = dtDuno.Rows[j][2].ToString() + "_" + dtDuno.Rows[j][3].ToString() + "_" + dtDuno.Rows[j][0].ToString();
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "tenkhachhang"))
 					{
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "tenkhachhang", dtDuno.Rows[j][2].ToString(), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("tenkhachhang", dtDuno.Rows[j][2].ToString());
 					}
 
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "diachi"))
 					{
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "LAV", dtDuno.Rows[j][52].ToString() + ", " + dtDuno.Rows[j][50].ToString() + ", " + dtDuno.Rows[j][48].ToString(), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace( "diachi", dtDuno.Rows[j][52].ToString() + ", " + dtDuno.Rows[j][50].ToString() + ", " + dtDuno.Rows[j][48].ToString());
 					}
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "LAV"))
 					{
-						dunohientai = Int32.Parse(dtDuno.Rows[j][17].ToString());
+						if (dtDuno.Rows[j][17].ToString().Trim() == "") break;
+						dunohientai = Int32.Parse(dtDuno.Rows[j][17].ToString());                      
 						LDS[0] = dtDuno.Rows[j][8].ToString();
 						giatri[0] = dtDuno.Rows[j][13].ToString();
 						mucdichvay[0] = dtDuno.Rows[j][58].ToString();
 						giatritt[0] = dtDuno.Rows[j][17].ToString();
-						for (int z = j + 1; z < dtDuno.Rows.Count; z++)
-						{
+						for (int z = j + 1; z < dtDuno.Rows.Count-1; z++)
+						{	if (dtDuno.Rows[z][3].ToString().Trim() == "") break;
 							if (dtDuno.Rows[j][3].ToString() == dtDuno.Rows[z][3].ToString())
 							{
 
 								count++;
-								dunohientai += Int32.Parse(dtDuno.Rows[z][17].ToString());
+								//MessageBox.Show(" dtDuno.Rows[j][3].ToString(): " + dtDuno.Rows[j][3].ToString() + " dtDuno.Rows[z][3].ToString(): " + dtDuno.Rows[z][3].ToString());
+								try
+								{
+									dunohientai += Int32.Parse(dtDuno.Rows[j][17].ToString());
+								}
+								catch (Exception )
+								{
+									dunohientai = 0;
+
+								}
 								LDS[count] = dtDuno.Rows[z][8].ToString();
 								giatri[count] = dtDuno.Rows[z][13].ToString();
 								mucdichvay[count] = dtDuno.Rows[z][58].ToString();
 								giatritt[count] = dtDuno.Rows[z][17].ToString();
-								
+								label2.Text = "LDS0: " + LDS[0] + " - giatri0: " + giatri[0] + " - " + " mucdichvay0: " + mucdichvay[0] + " - giatritt[0]: " + giatritt[0] + "\n";
+								label2.Text += "LDS1: " + LDS[count] + " - giatri1: " + giatri[count] + " - " + " mucdichvay1: " + mucdichvay[count] + " - giatritt[1]: " + giatritt[count] + "\n";
 
 							}
 							else break;
+							
 						}
-						label2.Text = dtDuno.Rows[j][8].ToString() + " - dunohientai: " + giatritt[0].ToString()
-									+ " - dunohopdong: " + giatri[0].ToString()
-									+ " - tongdunohientai: " + dunohientai;
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "LAV", dtDuno.Rows[j][3].ToString(), RegexOptions.IgnoreCase);
+					
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("LAV", dtDuno.Rows[j][3].ToString());
 					}
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "ngayvay"))
 					{
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "ngayvay", dtDuno.Rows[j][4].ToString(), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("ngayvay", dtDuno.Rows[j][4].ToString());
 
 					}
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "dunohientai"))
 					{
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "dunohientai", dunohientai.ToString("N0"), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("dunohientai", dunohientai.ToString("N0"));
 
 					}
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "sotiengiaingan"))
 					{
 						int dnhientai = Int32.Parse(dtDuno.Rows[j][7].ToString());
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "sotiengiaingan", dnhientai.ToString("N0"), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("sotiengiaingan", dnhientai.ToString("N0"));
 					}
 					else
 					if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "mucdichvay"))
 					{
-						doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "mucdichvay", dtDuno.Rows[j][58].ToString(), RegexOptions.IgnoreCase);
+						doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("mucdichvay", dtDuno.Rows[j][58].ToString());
 					}
 					else
 					{
@@ -120,24 +134,36 @@ namespace TSBD
 						{
 
 							if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "LDS" + (k + 1).ToString()))
-							{
-								label1.Text = "LDS" + (k + 1).ToString() + ": " + LDS[k].ToString();
-								doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "LDS" + (k + 1).ToString(), LDS[k].ToString(), RegexOptions.IgnoreCase);
+							{	
+								label1.Text += "LDS" + (k + 1).ToString() + ": " + LDS[k].ToString();
+								doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("LDS" + (k + 1).ToString(), LDS[k].ToString());
 							}
 							else if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "Mucdichvay" + (k + 1).ToString()))
 							{
-								label1.Text = "Mucdichvay" + (k + 1).ToString() + ": " + mucdichvay[k].ToString();
-								doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "Mucdichvay" + (k + 1).ToString(), mucdichvay[k].ToString(), RegexOptions.IgnoreCase);
+								
+								label1.Text += "Mucdichvay" + (k + 1).ToString() + ": " + mucdichvay[k].ToString();
+								doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("Mucdichvay" + (k + 1).ToString(), mucdichvay[k].ToString());
 							}
 							else if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "Giatri" + (k + 1).ToString()))
 							{
-								label1.Text = "Giatri" + (k + 1).ToString() + ": " + giatri[k].ToString();
-								doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "Giatri" + (k + 1).ToString(), giatri[k].ToString(), RegexOptions.IgnoreCase);
+								if (giatri[k].ToString() == "")
+								{
+									doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("Giatri" + (k + 1).ToString(), giatri[k].ToString());
+									continue;
+								}
+								label1.Text += "Giatri" + (k + 1).ToString() + ": " + giatri[k].ToString();
+								doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("Giatri" + (k + 1).ToString(),Int32.Parse(giatri[k].ToString()).ToString("N0"));
 							}
 							else if (Regex.IsMatch(doc.Paragraphs[i].Range.Text, "Dno" + (k + 1).ToString()))
 							{
-								label1.Text = "Du no " + (k + 1).ToString() + ": " + giatritt[k].ToString();
-								doc.Paragraphs[i].Range.Text = Regex.Replace(doc.Paragraphs[i].Range.Text, "Dno" + (k + 1).ToString(), giatritt[k].ToString(), RegexOptions.IgnoreCase);
+								if (giatritt[k].ToString() == "")
+								{
+									doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("Dno" + (k + 1).ToString(), giatritt[k].ToString());
+									continue;
+									
+								}
+									label1.Text += "Du no " + (k + 1).ToString() + ": " + giatritt[k].ToString()+"\n";
+								doc.Paragraphs[i].Range.Text = doc.Paragraphs[i].Range.Text.Replace("Dno" + (k + 1).ToString(), Int32.Parse(giatritt[k].ToString()).ToString("N0"));
 							}
 						}
 					}
@@ -146,9 +172,9 @@ namespace TSBD
 				 ((_Document)doc).SaveAs2(@"" + pa + "\\maubieu\\12A\\" + dtDuno.Rows[j][2].ToString() + "_" + dtDuno.Rows[j][3].ToString() + "_" + dtDuno.Rows[j][0].ToString() + ".docx");
 				//	else ((_Document)doc).SaveAs2(@"" + pa + "\\maubieu\\DN\\" + dtTaisan.Rows[j][2].ToString() + "_" + dtTaisan.Rows[j][1].ToString() + "_" + dtTaisan.Rows[j][5].ToString() + ".docx");
 				((_Document)doc).Close();
-				((_Application)word).Quit();
-				j = j + count;
-				if (j >= dtDuno.Rows.Count) break;
+				((_Application)word).Quit();			
+				j = j + count;				
+				if (j >= dtDuno.Rows.Count-1) break;
 			}
 			MessageBox.Show("OK XONG!");
 		}
